3b0d7b2540122ed729a78b781434a6244364f1ee
Adrian <adriantutuianu01@gmail.com>
2022-08-09 23:49:01 +0100

    Rendering question component

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,15 +1,17 @@
 import { useContext } from "react";
 import { QuizContext } from "../contexts/quiz";
+import Question from "./Question";
 
 const Quiz = () => {
   const [quizState, dispatch] = useContext(QuizContext);
   console.log("quizState", quizState);
   return (
-    <div>
+    <div className="quiz">
       <div className="score">
         Question {quizState.currentQuestionIndex + 1} /{" "}
         {quizState.questions.length}
       </div>
+      <Question />
     </div>
   );
 };
