f09d5b7ceae66e6219d029b890be4fb6417aca17
Adrian <adriantutuianu01@gmail.com>
2022-08-09 23:30:19 +0100

    Accesed quizState everywhere

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { QuizContext } from "../contexts/quiz";
+
 const Quiz = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+  console.log("quizState", quizState);
   return <div>Quiz</div>;
 };
 
