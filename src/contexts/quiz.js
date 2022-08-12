7aa5a9afd37f5cbf95cbd62fc5d9cb7d7acf0dd1
Adrian <adriantutuianu01@gmail.com>
2022-08-12 23:13:17 +0100

    Fixed error  - button next questions

--- src/contexts/quiz.js
+++ src/contexts/quiz.js
@@ -5,15 +5,22 @@ import questions from "../data";
 const initialState = {
   questions,
   currentQuestionIndex: 0,
+  showResults: false,
 };
 
 const reducer = (state, action) => {
   console.log("reducer", state, action);
   switch (action.type) {
     case "NEXT_QUESTION": {
+      const showResults =
+        state.currentQuestionIndex === state.questions.length - 1;
+      const currentQuestionIndex = showResults
+        ? state.currentQuestionIndex
+        : state.currentQuestionIndex + 1;
       return {
         ...state,
-        currentQuestionIndex: state.currentQuestionIndex + 1,
+        currentQuestionIndex,
+        showResults,
       };
     }
   }
